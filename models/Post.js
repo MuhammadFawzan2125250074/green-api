c619de923e2118faeab6f4dd46fa8d579cd37f92
MuhammadFawzan2125250074 <fawzan_mhd@mhs.mdp.ac.id>
2023-06-09 13:12:44 +0700

    .

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+    namapohon: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    alamat:{
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    modified_date: {
+        type: Date,
+        default: null
+    }
+}, {
+    versionKey: false
+})
+module.exports = mongoose.model('Post', postSchema, 'post')
